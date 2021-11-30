91bd7c1abcc6deb60777a08ec79f19842802a6d3
Ezhko <ucsd.camp.21@gmail.com>
2021-11-30 15:04:16 -0800

    fix types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,25 +4,27 @@ const typeDefs = gql`
   type Book {
     _id: ID
     bookId: String!
-    authors: String
+    authors: [String]
     title: String
     description: String
     image: String
+    link: String
   }
 
   input BookInput {
     bookId: String!
-    authors: String
+    authors: [String]
     title: String
     description: String
     image: String
+    link: String
   }
 
   type User {
     _id: ID
     username: String
     email: String
-    password: String
+    bookCount: Int
     savedBooks: [Book]
   }
 
